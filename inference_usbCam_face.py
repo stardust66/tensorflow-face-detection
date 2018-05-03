7cdd9b5af3d4d7d61114aa6883bdecba5575f6d3
Jason Chen <kbtpodifo@gmail.com>
2018-05-03 09:21:17 -0400

    Fix line lengths according to PEP8

--- inference_usbCam_face.py
+++ inference_usbCam_face.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 # pylint: disable=C0103
 # pylint: disable=E1101
@@ -18,7 +18,10 @@ PATH_TO_LABELS = './protos/face_label_map.pbtxt'
 NUM_CLASSES = 2
 
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+categories = label_map_util.convert_label_map_to_categories(
+    label_map,
+    max_num_classes=NUM_CLASSES,
+    use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
 class TensorflowFaceDetector():
