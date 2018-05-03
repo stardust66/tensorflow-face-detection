c387776afa81773548bf57672b608fb6420a1929
Jason Chen <kbtpodifo@gmail.com>
2018-05-03 09:22:33 -0400

    Make detector a context manager for closing sess
    Creates new session when entering

--- inference_usbCam_face.py
+++ inference_usbCam_face.py
@@ -40,14 +40,16 @@ class TensorflowFaceDetector():
                 old_graph_def.ParseFromString(serialized_graph)
                 tf.import_graph_def(old_graph_def, name='')
 
+        self.config = tf.ConfigProto()
+        self.config.gpu_options.allow_growth = True
+        self.windowNotSet = True
 
-        with self.detection_graph.as_default():
-            config = tf.ConfigProto()
-            config.gpu_options.allow_growth = True
-            with tf.Session(graph=self.detection_graph, config=config) as self.sess:
+    def __enter__(self):
+        self.sess = tf.Session(graph=self.detection_graph, config=self.config)
+        return self
 
-                self.windowNotSet = True
-
+    def __exit__(self, exec_type, exec_value, traceback):
+        self.sess.close()
 
     def run(self, image):
         """image: bgr image
@@ -77,49 +79,47 @@ class TensorflowFaceDetector():
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print """usage:%s (cameraID | filename)
-Detect faces in the video
-example:
-%s 0
-""" % (sys.argv[0], sys.argv[0])
-        exit(1)
+        print("usage:",
+              "{} [camera_number]".format(sys.argv[0]),
+              sep="\n")
+        sys.exit(1)
 
-    try:
-        camID = int(sys.argv[1])
-    except:
-        camID = sys.argv[1]
-
-    tDetector = TensoflowFaceDector(PATH_TO_CKPT)
-
+    camID = int(sys.argv[1])
     cap = cv2.VideoCapture(camID)
     windowNotSet = True
-    while True:
-        ret, image = cap.read()
-        if ret == 0:
-            break
 
-        [h, w] = image.shape[:2]
-        print h, w
-        image = cv2.flip(image, 1)
+    print("Starting detection, press q to quit...")
 
-        (boxes, scores, classes, num_detections) = tDetector.run(image)
+    with TensorflowFaceDetector(PATH_TO_CKPT) as tDetector:
+        while True:
+            ret, image = cap.read()
+            if ret == 0:
+                break
 
-        vis_util.visualize_boxes_and_labels_on_image_array(
-            image,
-            np.squeeze(boxes),
-            np.squeeze(classes).astype(np.int32),
-            np.squeeze(scores),
-            category_index,
-            use_normalized_coordinates=True,
-            line_thickness=4)
+            [h, w] = image.shape[:2]
+            image = cv2.flip(image, 1)
 
-        if windowNotSet is True:
-            cv2.namedWindow("tensorflow based (%d, %d)" % (w, h), cv2.WINDOW_NORMAL)
-            windowNotSet = False
+            start = time.time()
+            (boxes, scores, classes, num_detections) = tDetector.run(image)
+            elapsed = time.time() - start
+            print('inference time cost: {}'.format(elapsed))
 
-        cv2.imshow("tensorflow based (%d, %d)" % (w, h), image)
-        k = cv2.waitKey(1) & 0xff
-        if k == ord('q') or k == 27:
-            break
+            vis_util.visualize_boxes_and_labels_on_image_array(
+                image,
+                np.squeeze(boxes),
+                np.squeeze(classes).astype(np.int32),
+                np.squeeze(scores),
+                category_index,
+                use_normalized_coordinates=True,
+                line_thickness=4)
+
+            if windowNotSet:
+                cv2.namedWindow("Face Detector", cv2.WINDOW_NORMAL)
+                windowNotSet = False
+
+            cv2.imshow("Face Detector", image)
+            k = cv2.waitKey(1) & 0xff
+            if k == ord('q') or k == 27:
+                break
 
     cap.release()
